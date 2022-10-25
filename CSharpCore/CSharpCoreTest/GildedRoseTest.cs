af9434a88a28fc52ef7731b19a1d7aa2116bb584
Moritz Reis <moritz.reis@post.at>
2022-10-25 14:55:41 +0200

    addd test combinations

--- CSharpCore/CSharpCoreTest/GildedRoseTest.cs
+++ CSharpCore/CSharpCoreTest/GildedRoseTest.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using ApprovalTests.Reporters;
 using ApprovalTests;
 using Newtonsoft.Json;
+using ApprovalTests.Combinations;
 
 namespace CSharpCore.Test
 {
@@ -35,11 +36,34 @@ namespace CSharpCore.Test
         [UseReporter(typeof(VisualStudioReporter))]
         public void ShouldHaveItemWithExpectedName_VerifyJson()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
 
             Approvals.VerifyJson(JsonConvert.SerializeObject(Items[0]));
+        }
+
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void ShouldHaveItemWithAllCombinations()
+        {
+            IList<string> Items = new List<string> { "Aged Brie", 
+                                                     "Sulfuras, Hand of Ragnaros", 
+                                                     "Backstage passes to a TAFKAL80ETC concert", 
+                                                     "foo" };
+            IList<int> SellIns = new List<int> { -1, 0, 5, 6, 10, 11 };
+            IList<int> Qualities = new List<int> { 0, 49, 50, 51 };
+
+            CombinationApprovals.VerifyAllCombinations(doItemCheck, Items, SellIns, Qualities);
+        }
+
+        private string doItemCheck(string name, int sellIn, int quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            return JsonConvert.SerializeObject(Items[0]);
         }
     }
 }
