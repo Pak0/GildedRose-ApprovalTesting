82dcc448939cc32303b9596999a2cae251353f8e
Patrick Kovar <U00Q1BH@post.at>
2022-10-25 14:10:59 +0200

    initial commit

--- CSharpCore/CSharpCore/Item.cs
+++ CSharpCore/CSharpCore/Item.cs
@@ -5,5 +5,12 @@
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        public override string ToString() 
+        { 
+            return $"{Name}, {SellIn}, {Quality}";
+        }
     }
+
+    
 }
--- CSharpCore/CSharpCoreTest/GildedRoseTest.cs
+++ CSharpCore/CSharpCoreTest/GildedRoseTest.cs
@@ -1,20 +1,46 @@
 using Xunit;
 using System.Collections.Generic;
 using FluentAssertions;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+using Newtonsoft.Json;
 
 namespace CSharpCore.Test
 {
     public class GildedRoseTest
     {
         [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
         public void ShouldHaveItemWithExpectedName()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo2", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            //Items[0].Name.Should().Be("foo");
+            Approvals.Verify(Items[0].ToString());
+        }
+
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void ShouldHaveItemWithExpectedName_JsonConvert()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
 
-            Items[0].Name.Should().Be("foo");
+            Approvals.Verify(JsonConvert.SerializeObject(Items[0]));
         }
 
+        [Fact]
+        [UseReporter(typeof(VisualStudioReporter))]
+        public void ShouldHaveItemWithExpectedName_VerifyJson()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Approvals.VerifyJson(JsonConvert.SerializeObject(Items[0]));
+        }
     }
 }
